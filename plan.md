ad1f6cbdc9c5c8208feead6807cc7f7a3fed6443
nikhil <sandytany100@gmail.com>
2021-11-14 00:55:39 +0530

    First Commit

---
+++ plan.md
@@ -0,0 +1,9 @@
+# things to do
+
+- Initialize Our NodeJS DONE
+  -Initialize our first view DONE
+- Creating a Room DONE
+- Enabling a video
+- Add ability to allow other to stream their videos too
+- add styling
+- sending message
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require ('express');
+const { ExpressPeerServer } = require('peer');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid');
+const peerServer =  ExpressPeerServer(server, {
+    debug: true
+});
+
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', {roomId: req.params.room })
+});
+
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+      socket.join(roomId)
+      socket.broadcast.to(roomId).emit('user-connected', userId);
+      // messages
+      socket.on('message', (message) => {
+        //send message to the same room
+        io.to(roomId).emit('createMessage', message)
+    }); 
+  
+      socket.on('disconnect', () => {
+        socket.broadcast.to(roomId).emit('user-disconnected', userId)
+      })
+    })
+  })
+
+
+server.listen(3030);
+
+//To run this project use nodemon server.js on your terminal after npm i (3:10)
